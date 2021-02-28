d1c18fb3649c74e93704f986021455fb13d66ff2
Kevin Scardina <kscardina@gmail.com>
2021-02-27 19:19:57 -0700

    better coloring on svg

--- cov.sh
+++ cov.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+GIT_HASH=`git rev-parse --short HEAD`
+[ $? != 0 ] && rm -rf "${COVERAGE_GIST}" && exit 1
+echo "► current commit hash: ${GIT_HASH}"
+
 if [[ " $@ " =~ " +all " ]]; then
     echo "► linting..."
     swiftlint --strict
@@ -35,21 +39,23 @@ REGEX_INSTANTIATIONS='"totals"\s*:\s*\{.*?"instantiations"\s*:\s*.*?"percent"\s*
 REGEX_REGIONS='"totals"\s*:\s*\{.*?"regions"\s*:\s*.*?"percent"\s*:([0-9\.]*)'
 COVFILE=".build/debug/codecov/swift-httprequest.json"
 
-PERCENT_LINES=`pcregrep -o1 "$REGEX_LINES" "$COVFILE" | cut -d'.' -f1`
+PERCENT_LINES=`printf '%.2f' $(pcregrep -o1 "$REGEX_LINES" "$COVFILE")`
 [ $? != 0 ] && exit 1
-PERCENT_FUNCTIONS=`pcregrep -o1 "$REGEX_FUNCTIONS" "$COVFILE" | cut -d'.' -f1`
+PERCENT_FUNCTIONS=`printf '%.2f' $(pcregrep -o1 "$REGEX_FUNCTIONS" "$COVFILE")`
 [ $? != 0 ] && exit 1
-PERCENT_INSTANTIATIONS=`pcregrep -o1 "$REGEX_INSTANTIATIONS" "$COVFILE" | cut -d'.' -f1`
+PERCENT_INSTANTIATIONS=`printf '%.2f' $(pcregrep -o1 "$REGEX_INSTANTIATIONS" "$COVFILE")`
 [ $? != 0 ] && exit 1
-PERCENT_REGIONS=`pcregrep -o1 "$REGEX_REGIONS" "$COVFILE" | cut -d'.' -f1`
+PERCENT_REGIONS=`printf '%.2f' $(pcregrep -o1 "$REGEX_REGIONS" "$COVFILE")`
 [ $? != 0 ] && exit 1
+
 echo "   ► totals:"
-echo "     ★ instantiations: ${PERCENT_INSTANTIATIONS}%"
-echo "     ★ functions: ${PERCENT_FUNCTIONS}%"
-echo "     ★ lines: ${PERCENT_LINES}%"
-echo "     ★ regions: ${PERCENT_REGIONS}%"
+echo "      ★ instantiations: ${PERCENT_INSTANTIATIONS}%"
+echo "      ★ functions: ${PERCENT_FUNCTIONS}%"
+echo "      ★ lines: ${PERCENT_LINES}%"
+echo "      ★ regions: ${PERCENT_REGIONS}%"
 
 if [[ " $@ " =~ " +update_badge " ]]; then
+    echo "► uploading badges:"
     TMPL='<svg  xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     width="128"
@@ -60,7 +66,7 @@ if [[ " $@ " =~ " +update_badge " ]]; then
     height="100%" 
     width="100%"
     stroke="#0a700a"
-    fill="gray"
+    fill="#8c1515"
     style="
         stroke-width: 2;
         rx: 8;
@@ -107,37 +113,35 @@ if [[ " $@ " =~ " +update_badge " ]]; then
 {{PERCENT}}%
 </text>
 </svg>'
-    GIT_HASH=`git rev-parse --short HEAD`
-    [ $? != 0 ] && rm -rf "${COVERAGE_GIST}" && exit 1
-    echo "   ► current hash: ${GIT_HASH}"
     
     PERCENTS=("${PERCENT_INSTANTIATIONS}" "${PERCENT_FUNCTIONS}" "${PERCENT_LINES}" "${PERCENT_REGIONS}")
     LABELS=("instantiations" "functions" "lines" "regions")
     GISTS=("91a7a42d5a8b205ed4d4da6553969aa7" "66bf591f9bf0903867893afad30b8b2c" "85d803a29268ce9ae5a6e59f3d8f7882" "ac14c03f4beef83001796db0c3a4c112")
     COVERAGE_GIST=coverage.gist
     for idx in {0..3}; do
+        echo "   ► uploading badge for ${LABELS[$idx]}..."
         GIST_URL="git@gist.github.com:${GISTS[$idx]}.git"
-        echo "   ► cloning gist ${GIST_URL} into ${COVERAGE_GIST}..."
-        git clone "${GIST_URL}" "${COVERAGE_GIST}"
+        echo "      ► cloning gist ${GIST_URL} into ${COVERAGE_GIST}..."
+        git clone "${GIST_URL}" "${COVERAGE_GIST}" > /dev/null 2> /dev/null
         [ $? != 0 ] && exit 1
         
         SVG="swift-httprequesting-${LABELS[$idx]}-coverage.svg"
-        echo "   ► creating SVG ${SVG}..."
+        echo "      ► creating SVG ${SVG}..."
         
         SVG_PATH="${COVERAGE_GIST}/${SVG}"
         echo "$TMPL" | sed 's/{{PERCENT}}/'"${PERCENTS[$idx]}"'/' > "${SVG_PATH}"
         [ $? != 0 ] && rm -rf "${COVERAGE_GIST}" && exit 1
-        echo "   ► created SVG: \"${SVG_PATH}\""
+        echo "      ► created SVG: \"${SVG_PATH}\""
 
-        echo "   ► push gist..."
+        echo "      ► push badge ${LABELS[$idx]}..."
         pushd "${COVERAGE_GIST}" > /dev/null
         [ $? != 0 ] && rm -rf "${COVERAGE_GIST}" && exit 1
-        git commit -am "coverage update: ${GIT_HASH}"
-        git push origin master
+        echo "      ► set commit \"coverage update: ${GIT_HASH}\""
+        git commit -am "coverage update: ${GIT_HASH}" > /dev/null 2> /dev/null
+        git push origin master > /dev/null 2> /dev/null
         popd > /dev/null
         [ $? != 0 ] && rm -rf "${COVERAGE_GIST}" && exit 1
         rm -rf "${COVERAGE_GIST}"
+        echo "      ► clean up: ${COVERAGE_GIST}"
     done
 fi
-
-echo "► coveraged generated"
